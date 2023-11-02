e5810fcf23017223d576e82ad7b0bfb363b21af0
Ravi Dholariya <85175451+Ravi-Dholariya@users.noreply.github.com>
2023-11-03 00:54:21 +0530

    issue reslove of crashing

--- index.js
+++ index.js
@@ -44,11 +44,13 @@ io.on("connection", (socket) => {
   });
   socket.on("sendMessage", (message, callback) => {
     const user = getUser(socket.id);
-    io.to(user.room).emit("message", { user: user.name, text: message });
-    io.to(user.room).emit("roomData", {
-      room: user.room,
-      users: getUsersInRoom(user.room),
-    });
+    if (user) {
+      io.to(user.room).emit("message", { user: user.name, text: message });
+      io.to(user.room).emit("roomData", {
+        room: user.room,
+        users: getUsersInRoom(user.room),
+      });
+    }
     callback();
   });
   socket.on("disconnect", () => {
